b72ccc14b4dfcab528526be8b10bf6b1680f07ac
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-04-12 20:54:16 +0100

    new color merge

--- src/merge_images.py
+++ src/merge_images.py
@@ -1,19 +1,16 @@
 import sys
-from PIL import Image as PillowImage
-from PIL import ImageEnhance as PillowEnhance
+import numpy as np
 from pathlib import Path
+from PIL import Image as PillowImage
+from skimage.exposure import exposure
 
 def merge_images(landscapePath, cloudPath):
-    landscape = PillowImage.open(landscapePath).convert("RGBA")
-    clouds = PillowImage.open(cloudPath).convert("RGBA")
-    output = PillowImage.blend(landscape, clouds, 0.5)
+	landscape = PillowImage.open(landscapePath).convert("RGBA")
+	clouds = PillowImage.open(cloudPath).convert("RGBA")
+	output = PillowImage.blend(landscape, clouds, 0.5)
 
-    brightnessEnhancement = PillowEnhance.Brightness(output)
-    output = brightnessEnhancement.enhance(1.75)
+	output = output.convert('RGB')
+	output = exposure.rescale_intensity(output, out_range=(0, 255))
+	output = PillowImage.fromarray(np.uint8(output))
 
-    colorBoost = PillowEnhance.Color(output)
-    output = colorBoost.enhance(1.25)
-
-    output = output.convert('RGB')
-
-    return output
+	return output
